c19514545c5db79e981e3538c627bb7a5f0a61db
rzstead <kingstead1@gmail.com>
2017-04-18 11:10:10 -0600

    More GUI changes, in xaml.cs.

--- GUILanguageAgnosticIDE/MainGUIWindow.xaml.cs
+++ GUILanguageAgnosticIDE/MainGUIWindow.xaml.cs
@@ -36,12 +36,47 @@ namespace GUILanguageAgnosticIDE
 		public OutputFactory FileOutputFactory { get; set; }
 		public MainGUIWindow(OutputFactory outputFactory, List<string> availableLanguages)
         {
+            CurrentLanguage = "html";
             InitializeComponent();
-			FileOutputFactory = outputFactory;
-			AvailableLanguages = availableLanguages;
+            FileOutputFactory = outputFactory;
+            AvailableLanguages = availableLanguages;
+            //for each available element, create a label with the name of the element type.
+            //Add to combo box.
+            //Subscribe each label to the onClick handler.
+            PopulateInfo();
         }
 
-		public void RunGUI()
+        private void PopulateInfo()
+        {
+            LangCombo.Items.Add("Output Language");
+            ElementDropDown.HorizontalContentAlignment = HorizontalAlignment.Center;
+            foreach (string al in AvailableLanguages)
+            {
+                LangCombo.Items.Add(al);
+            }
+            foreach (string s in AvailableElements)
+            {
+                Label label = new Label();
+                label.MouseDown += ElementClickHandler;
+                label.Height = 60;
+                label.Margin = new Thickness(5);
+                label.Background = Brushes.DarkGray;
+                label.Content = s;
+                label.HorizontalContentAlignment = HorizontalAlignment.Center;
+                label.VerticalContentAlignment = VerticalAlignment.Center;
+                label.FontSize = 24;
+                label.Width = 240;
+
+                ElementDropDown.Items.Add(label);
+            }
+        }
+
+        private void ElementClickHandler(object sender, MouseButtonEventArgs e)
+        {
+           //((Label)e.Source).
+        }
+
+        public void RunGUI()
 		{
 			Output output = FileOutputFactory.CreateOutput(CurrentLanguage);
 			foreach(Element element in Elements)
