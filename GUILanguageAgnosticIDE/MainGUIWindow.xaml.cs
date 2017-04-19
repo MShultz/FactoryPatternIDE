f51a4d8f7727a71ebe2007121deec19321a52694
rzstead <kingstead1@gmail.com>
2017-04-18 18:57:47 -0600

    Fecking everything. Gui is done. DLL is now complete.

--- GUILanguageAgnosticIDE/MainGUIWindow.xaml.cs
+++ GUILanguageAgnosticIDE/MainGUIWindow.xaml.cs
@@ -22,41 +22,59 @@ namespace GUILanguageAgnosticIDE
     /// </summary>
     public partial class MainGUIWindow : UserControl
     {
-		public string CurrentLanguage { get; set; }
-		public List<string> AvailableLanguages { get; set; }
-        public List<Label> ElementsOnPage { get; set;}
-		public List<string> AvailableElements
-		{
-			get
-			{
-				return FileOutputFactory.CreateAvailableElementList(CurrentLanguage);
-			}
-		}
-		public List<AgnosticElement> Elements { get; set; }
-		public OutputFactory FileOutputFactory { get; set; }
-		public MainGUIWindow(OutputFactory outputFactory, List<string> availableLanguages)
+        public string CurrentLanguage { get; set; }
+        public List<string> AvailableLanguages { get; set; }
+        public List<Label> ElementsOnPage { get; set; }
+        private string SelectedElement;
+        private int ElementCounter;
+        public List<string> AvailableElements
+        {
+            get
+            {
+                return FileOutputFactory.CreateAvailableElementList(CurrentLanguage);
+            }
+        }
+        public List<AgnosticElement> Elements { get; set; }
+        public OutputFactory FileOutputFactory { get; set; }
+        public MainGUIWindow(OutputFactory outputFactory, List<string> availableLanguages)
         {
             InitializeComponent();
             FileOutputFactory = outputFactory;
             AvailableLanguages = availableLanguages;
-			CurrentLanguage = AvailableLanguages[0];
+            CurrentLanguage = AvailableLanguages[0];
             Setup();
         }
 
         private void Setup()
         {
             GuiCanvas.MouseDown += CanvasClickHandler;
-            ElementDropDown.HorizontalContentAlignment = HorizontalAlignment.Center;
-            ElementDropDown.SelectionChanged += LanguageChangeHandler;
+            Remove.Click += RemoveElements;
+            ElementListView.HorizontalContentAlignment = HorizontalAlignment.Center;
+            Elements = new List<AgnosticElement>();
+            ElementsOnPage = new List<Label>();
+            Compile.Click += RunGUI;
             foreach (string languages in AvailableLanguages)
             {
                 LangCombo.Items.Add(languages);
             }
-                LangCombo.SelectedItem = LangCombo.Items.GetItemAt(0);
+            LangCombo.SelectedItem = LangCombo.Items.GetItemAt(0);
+            LangCombo.SelectionChanged += LanguageChangeHandler;
+            CreateLablesForLanguage();
+        }
+
+        private void RemoveElements(object sender, RoutedEventArgs e)
+        {
+            GuiCanvas.Children.RemoveAt(GuiCanvas.Children.Count - 1);
+            Elements.RemoveAt(Elements.Count - 1);
+            ElementsOnPage.RemoveAt(ElementsOnPage.Count - 1);
+        }
+
+        private void CreateLablesForLanguage()
+        {
             foreach (string s in AvailableElements)
             {
                 Label label = new Label();
-                label.MouseDown += ElementClickHandler;
+                label.MouseLeftButtonDown += ElementClickHandler;
                 label.Height = 60;
                 label.Margin = new Thickness(5);
                 label.Background = Brushes.DarkGray;
@@ -65,34 +83,69 @@ namespace GUILanguageAgnosticIDE
                 label.VerticalContentAlignment = VerticalAlignment.Center;
                 label.FontSize = 24;
                 label.Width = 240;
-                ElementDropDown.Items.Add(label);
+                ElementListView.Items.Add(label);
             }
         }
 
         private void CanvasClickHandler(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            Dialogue dialoguePrompt = new Dialogue();
+            double mouseX = e.GetPosition(GuiCanvas).X;
+            double mouseY = e.GetPosition(GuiCanvas).Y;
+            if (dialoguePrompt.ShowDialog() == true)
+            {
+                Label canvasLabel = new Label();
+                canvasLabel.Name = SelectedElement + ElementCounter++ + "";
+                canvasLabel.Content = dialoguePrompt.ResponseText;
+                canvasLabel.Height = dialoguePrompt.HeightText;
+                canvasLabel.Width = dialoguePrompt.WidthText;
+                canvasLabel.Background = Brushes.LightGray;
+                canvasLabel.BorderThickness = new Thickness(2);
+                canvasLabel.BorderBrush = Brushes.Black;
+                canvasLabel.HorizontalContentAlignment = HorizontalAlignment.Center;
+                canvasLabel.VerticalContentAlignment = VerticalAlignment.Center;
+                ElementsOnPage.Add(canvasLabel);
+
+                Elements.Add(new AgnosticElement(SelectedElement, canvasLabel.Content.ToString(), (int)mouseY, (int)mouseX, (int)canvasLabel.Height, (int)canvasLabel.Width));
+
+                GuiCanvas.Children.Add(canvasLabel);
+                Canvas.SetTop(canvasLabel, mouseY);
+                Canvas.SetLeft(canvasLabel, mouseX);
+            }
         }
 
         private void LanguageChangeHandler(object sender, SelectionChangedEventArgs e)
         {
-            CurrentLanguage = ((string)e.Source);
-            //ElementDropDown.SelectedItem = CurrentLanguage;
+            CurrentLanguage = LangCombo.SelectedItem.ToString();
+            FileOutputFactory.CreateAvailableElementList(CurrentLanguage);
+            ElementListView.Items.Clear();
+            CreateLablesForLanguage();
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                if (!AvailableElements.Contains(Elements[i].GetElementData()))
+                {
+                    //wonky, change if you're not lazy.
+                    MessageBox.Show("Will remove all instances of the element of type \"" + Elements[i].GetElementData() + "\"");
+                    GuiCanvas.Children.Remove(ElementsOnPage[i]);
+                    Elements.RemoveAt(i);
+                    ElementsOnPage.RemoveAt(i);
+                }
+            }
         }
 
-        private void ElementClickHandler(object sender, MouseButtonEventArgs e)
+        private void ElementClickHandler(object sender, RoutedEventArgs e)
         {
-           //((Label)e.Source).
+            SelectedElement = ((Label)e.Source).Content.ToString();
         }
 
-        public void RunGUI()
-		{
-			Output output = FileOutputFactory.CreateOutput(CurrentLanguage);
-			foreach(Element element in Elements)
-			{
-				output.AddElement(element.GetElementData(), element.Content, element.Top, element.Left, element.Height, element.Width);
-			}
-			output.CompileFile();
-		}
+        public void RunGUI(object s, RoutedEventArgs e)
+        {
+            Output output = FileOutputFactory.CreateOutput(CurrentLanguage);
+            foreach (Element element in Elements)
+            {
+                output.AddElement(element.GetElementData(), element.Content, element.Top, element.Left, element.Height, element.Width);
+            }
+            output.CompileFile();
+        }
     }
 }
