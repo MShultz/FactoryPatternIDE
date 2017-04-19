ec2fc89eefccd9ed361ecc372359780c9819d9af
rzstead <kingstead1@gmail.com>
2017-04-18 19:00:43 -0600

    Added The dialogue window

---
+++ GUILanguageAgnosticIDE/Dialogue.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUILanguageAgnosticIDE
+{
+    /// <summary>
+    /// Interaction logic for Dialogue.xaml
+    /// </summary>
+    public partial class Dialogue : Window
+    {
+        public Dialogue()
+        {
+            InitializeComponent();
+        }
+
+        public string ResponseText
+        {
+            get { return ResponseTextBox.Text; }
+            set { ResponseTextBox.Text = value; }
+        }
+        public int HeightText
+        {
+            get { return int.Parse(HeightTextBox.Text); }
+            set { HeightTextBox.Text = value + ""; }
+        }
+        public int WidthText
+        {
+            get { return int.Parse(WidthTextBox.Text); }
+            set { WidthTextBox.Text = value + ""; }
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
